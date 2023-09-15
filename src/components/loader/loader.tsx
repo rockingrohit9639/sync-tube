f8d8a9068a34e4b8d2b5db4cf9f17850c2c6b315
= <rohitbhatwara1@gmail.com>
2023-09-15 08:42:41 +0530

    feat(projects): add loading page for projects dashboard

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -9,7 +9,7 @@ type LoaderProps = {
 
 export default function Loader({ className, style, title }: LoaderProps) {
   return (
-    <div className={cn('z-50 flex h-screen w-full items-center justify-center', className)} style={style}>
+    <div className={cn('fixed inset-0 z-50 flex h-screen w-full items-center justify-center', className)} style={style}>
       <LoadingIcon className="mr-2 h-6 w-6 animate-spin" />
       <span className="text-sm text-muted-foreground">{title ?? 'Loading...'}</span>
     </div>
