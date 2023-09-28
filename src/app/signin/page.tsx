a83a779fc99f777f04d0a4abd0a8f2589de0564b
= <rohitbhatwara1@gmail.com>
2023-09-28 09:38:48 +0530

    feat(login): update login page styles

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { VideoIcon, Youtube } from 'lucide-react'
 import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import Loader from '~/components/loader'
 import { Button } from '~/components/ui/button'
@@ -18,30 +20,71 @@ export default function Signin() {
   }
 
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-gradient-to-b from-slate-800 to-slate-950 p-8 text-white">
-      <div className="mx-auto flex w-full max-w-screen-md flex-col items-center justify-center rounded-md p-4">
-        <Image className="h-28 w-28 rounded-full object-cover" width={200} height={200} src="/logo.png" alt="logo" />
-        <div className="mb-4">
-          Login to <span className="text-lg font-bold text-foreground">SyncTube</span>
-        </div>
+    <div className="container relative grid h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Welcome to SyncTube</h1>
+            <p className="text-sm text-muted-foreground">Streamlining Collaboration for YouTubers and Editors</p>
+          </div>
 
-        <div className="flex flex-col space-y-4">
-          <Button
-            className="bg-slate-800 text-gray-100 hover:bg-slate-700"
-            onClick={() => {
-              signIn('youtube')
-            }}
-          >
-            Continue as Youtuber
-          </Button>
-          <Button
-            className="bg-slate-800 text-gray-100 hover:bg-slate-700"
-            onClick={() => {
-              signIn('editor')
-            }}
-          >
-            Continue as Editor
-          </Button>
+          <div className="grid gap-6">
+            <Button
+              onClick={() => {
+                signIn('youtube')
+              }}
+            >
+              <Youtube className="mr-2 h-4 w-4" />
+              Continue as Youtuber
+            </Button>
+
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">Or</span>
+              </div>
+            </div>
+
+            <Button
+              variant="outline"
+              onClick={() => {
+                signIn('editor')
+              }}
+            >
+              <VideoIcon className="mr-2 h-4 w-4" />
+              Continue as Editor
+            </Button>
+          </div>
+
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{' '}
+            <Link href="/terms" className="underline underline-offset-4 hover:text-primary">
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link href="/privacy" className="underline underline-offset-4 hover:text-primary">
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <Image
+          src="/login-bg.jpg"
+          width={1200}
+          height={600}
+          alt="background"
+          className="absolute inset-0 h-full w-full bg-zinc-900 object-cover opacity-60"
+        />
+        <div className="relative z-20 mt-auto">
+          <p className="text-lg">
+            SyncTube is your go-to platform for hassle-free video editing collaboration. Say goodbye to the complexity
+            and streamline your workflow.
+          </p>
         </div>
       </div>
     </div>
